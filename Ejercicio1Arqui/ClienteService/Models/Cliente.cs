7dc2904981ed278cf255b36130ea68ba661f1f13
miguelbay <168833100+miguelbay@users.noreply.github.com>
2025-05-04 18:40:12 -0500

    Delete Ejercicio1Arqui directory

--- Ejercicio1Arqui/ClienteService/Models/Cliente.cs
+++
@@ -1,7 +0,0 @@
-public class Cliente
-{
-    public string? Cedula { get; set; }
-    public string? Nombre { get; set; }
-    public string? Direccion { get; set; }
-    public string? Telefono { get; set; }
-}
